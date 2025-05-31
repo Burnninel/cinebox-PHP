d53c0d17924d467929d638b8e8f6bd262d3fe7db
Burnninel <brunoabc11@gmail.com>
2025-05-31 19:46:35 -0300

    Adicionando função config() no Helpers.php
    Responsável por carregar o array de configuração do banco de dados e retornar uma chave específica ou todo o conteúdo.

--- src/core/Helpers.php
+++ src/core/Helpers.php
@@ -7,3 +7,14 @@ function dd($data)
     echo '</pre>';
     die();
 }
+
+function config($chave = null)
+{
+    $config = require __DIR__ . '/../../config/config.php';
+
+    if(strlen($chave) > 0) {
+        return $config[$chave];
+    }
+
+    return $config;
+}
