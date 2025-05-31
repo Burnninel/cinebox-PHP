ca0b26820f9bec33a6301a5ef1b3f911e2aa128d
Burnninel <brunoabc11@gmail.com>
2025-05-31 19:48:19 -0300

    adicionando o config/config.php com dados de conexão ao banco
    Arquivo retorna array com configurações de acesso ao banco de dados MySQL, como host, dbname, usuário, senha e charset.

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'database' => [
+        'driver' => 'mysql',
+
+        'host' => 'localhost',
+        'dbname' => 'cinebox',
+        'username' => 'root',
+        'password' => '',   
+        'charset' => 'utf8',
+    ]
+];
