0fd8423862d5dfabd3b9c6f0fb0d29eb175f4eb5
Burnninel <brunoabc11@gmail.com>
2025-05-31 22:07:58 -0300

    Integração do controller Filme com consulta ao banco de dados
    Atualizado o método index do FilmeController para buscar dados do banco utilizando injeção de dependência do Database e mapeamento para o model Filme.

--- src/controllers/FilmeController.php
+++ src/controllers/FilmeController.php
@@ -2,9 +2,9 @@
 
 class FilmeController extends Controller
 {
-    public function index()
+    public function index($database)
     {
-        $filmes = ['Matrix'];
+        $filmes = $database->query("SELECT * FROM filmes WHERE id = :id", Filme::class, ['id' => $_GET['id']])->fetch();
         $this->view('filme/index', ['filmes' => $filmes]);
     }
 }
