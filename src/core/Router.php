22971c2a5356fe86eff4486510470f379f1a97a6
Burnninel <brunoabc11@gmail.com>
2025-06-01 17:23:10 -0300

    Reestruturação inicial do Router com base simples
    Cria estrutura base de rotas usando array associativo. Substitui verificações manuais por mapeamento dinâmico de controllers. Adiciona tratamento de rota não encontrada (404). Estrutura ainda é simples, servindo como base para futuras melhorias.

--- src/core/Router.php
+++ src/core/Router.php
@@ -1,13 +1,19 @@
 <?php 
 
-$rota = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
+$path = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
 
-if($rota === 'filme') {
-    $controller = new FilmeController();
-    $controller->index();   
-} elseif ($rota === 'login') {
-    $controller = new FilmeController();
-    $controller->index();
-} else {
+$routes = [
+    '' => 'IndexController',
+    'filme' => 'FilmeController',
+];
+
+if(!array_key_exists($path, $routes)) {
+    http_response_code(404);
     echo "Rota não encontrada.";
+    exit;
 }
+
+$controllerName = $routes[$path];
+
+$controller = new $controllerName();
+$controller->index($database);   
