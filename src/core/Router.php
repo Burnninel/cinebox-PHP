b8371fa5640fc27d755101c11e0c65da715509b6
Burnninel <brunoabc11@gmail.com>
2025-06-18 23:45:55 -0300

    Refatora router.php para suportar ações dinâmicas nas rotas
    Substitui parsing simples da rota por split em rota e ação, adiciona método padrão 'index', remove rota 'meus-filmes' e adapta chamada do controller para executar método dinâmico conforme ação.

--- src/core/Router.php
+++ src/core/Router.php
@@ -1,22 +1,22 @@
 <?php 
 
-$path = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
+$uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+[$rota, $acao] = array_pad(explode('/', $uri, 2), 2, 'index');
 
 $routes = [
     '' => 'IndexController',
     'filme' => 'FilmeController',
     'login' => 'LoginController',
     'registrar' => 'RegistrarController',
-    'meus-filmes' => 'UsuarioFilmesController',
 ];
 
-if(!array_key_exists($path, $routes)) {
+if(!array_key_exists($rota, $routes)) {
     http_response_code(404);
     echo "Rota não encontrada.";
     exit;
 }
 
-$controllerName = $routes[$path];
+$controllerName = $routes[$rota];
+$controller = new $controllerName($database);
 
-$controller = new $controllerName();
-$controller->index($database);   
+$controller->$acao($database);
