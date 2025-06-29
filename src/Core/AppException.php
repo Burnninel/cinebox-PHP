d2ae3571f0e87827830fdeafa707d0d2f4a3a079
Burnninel <brunoabc11@gmail.com>
2025-06-29 00:45:08 -0300

    Renomear pastas e namespaces para PascalCase

--- src/Core/AppException.php
+++ src/Core/AppException.php
@@ -6,13 +6,13 @@ class AppException extends \Exception
 {
     protected $detalhesDb;
 
-    public function __construct($mensagem, $detalhesDb = '', $codigo = 500)
+    public function __construct(string $mensagem, string $detalhesDb = '', int $codigo = 500)
     {
         parent::__construct($mensagem, $codigo);
         $this->detalhesDb = $detalhesDb;
     }
 
-    public function getDetalhes()
+    public function getDetalhes() : string
     {
         return $this->detalhesDb;
     }
